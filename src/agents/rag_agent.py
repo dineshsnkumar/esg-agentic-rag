a84a9a290ab57e12ae4feeb9b1d2665bfafc97d8
Dinesh <dinesh@Dineshs-MacBook-Air.local>
2026-04-18 14:53:02 +0530

    Creating indexs and storing in chromadb

--- src/agents/rag_agent.py
+++ src/agents/rag_agent.py
@@ -1,7 +1,14 @@
 # type: ignore
 
 import os
+import chromadb
 from llama_parse import LlamaParse
+from llama_index.core.node_parser import SentenceSplitter
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.vector_stores.chroma import ChromaVectorStore
+from llama_index.core.ingestion import IngestionPipeline
+from llama_index.core import VectorStoreIndex
+from llama_index.core import StorageContext
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,7 +17,6 @@ llama_api_key = os.getenv("LLAMA_CLOUD_API_KEY")
 hf_token = os.getenv("HF_TOKEN")
 
 # 1. Parse the pdf documents
-
 parser = LlamaParse(
    api_key= llama_api_key,  
    result_type="markdown",
@@ -23,7 +29,23 @@ extra_info = {"file_name": sustainability_report}
 #Parse documents
 parsed_documents = parser.load_data(sustainability_report, extra_info=extra_info)
 
-# Saving the parsed documents
-with open("output.md", "w", encoding="utf-8") as f:
-   for doc in parsed_documents:
-       f.write(doc.text)
+
+# 2. Indexing and storing
+
+splitter = SentenceSplitter(chunk_size=1024)
+nodes = splitter.get_nodes_from_documents(parsed_documents)
+
+db = chromadb.PersistentClient(path="./esg_audit_db")
+chroma_collection = db.get_or_create_collection("ms_esg")
+vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+storage_context = StorageContext.from_defaults(vector_store=vector_store)
+
+embed_model = HuggingFaceEmbedding("BAAI/bge-small-en-v1.5")
+
+
+index = VectorStoreIndex.from_documents(
+    parsed_documents, 
+    storage_context=storage_context,
+    embed_model = embed_model
+)
+print(index)
