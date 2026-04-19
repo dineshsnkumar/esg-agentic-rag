32842ad26c0f9fde2487b5112e23b7462ee8545c
Dinesh <dinesh@Dineshs-MacBook-Air.local>
2026-04-19 13:35:29 +0530

    Added a Summary tool

--- src/agents/rag_agent.py
+++ src/agents/rag_agent.py
@@ -5,16 +5,26 @@ import chromadb
 from llama_parse import LlamaParse
 from llama_index.core.node_parser import SentenceSplitter
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.llms.huggingface_api import HuggingFaceInferenceAPI
 from llama_index.vector_stores.chroma import ChromaVectorStore
 from llama_index.core.ingestion import IngestionPipeline
 from llama_index.core import VectorStoreIndex
 from llama_index.core import StorageContext
+from llama_index.core import SummaryIndex
+from llama_index.core.tools import QueryEngineTool
+from llama_index.core import Settings
 from dotenv import load_dotenv
 
 load_dotenv()
 
 llama_api_key = os.getenv("LLAMA_CLOUD_API_KEY")
 hf_token = os.getenv("HF_TOKEN")
+
+# Setting the embeddig and the llm
+embed_model = HuggingFaceEmbedding("BAAI/bge-small-en-v1.5")
+llm = HuggingFaceInferenceAPI(model_name="Qwen/Qwen2.5-Coder-32B-Instruct", embed_model = embed_model)
+Settings.embed_model = embed_model
+Settings.llm = llm
 
 # 1. Parse the pdf documents
 parser = LlamaParse(
@@ -40,12 +50,37 @@ chroma_collection = db.get_or_create_collection("ms_esg")
 vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
 storage_context = StorageContext.from_defaults(vector_store=vector_store)
 
-embed_model = HuggingFaceEmbedding("BAAI/bge-small-en-v1.5")
+if chroma_collection.count() >  0:
+    index = VectorStoreIndex.from_vector_store(
+        vector_store, 
+        storage_context=storage_context,
+        embed_model=embed_model
+    )
+    print(f"Loaded existing index from ChromaDB and index is {index}")
+    
+else: 
+    index = VectorStoreIndex.from_documents(
+        parsed_documents, 
+        storage_context=storage_context,
+        embed_model = embed_model
+    )
 
 
-index = VectorStoreIndex.from_documents(
-    parsed_documents, 
-    storage_context=storage_context,
-    embed_model = embed_model
+# 3. Querying using the tools
+
+summary_index = SummaryIndex(nodes=nodes, embed_model= embed_model)
+summary_query_engine = summary_index.as_query_engine(
+    response_mode="tree_summarize",
+    use_async=True
 )
-print(index)
+summary_tool = QueryEngineTool.from_defaults(
+    name="summary_tool",
+    query_engine=summary_query_engine
+)
+
+response = llm.predict_and_call(
+    user_msg="Summarize this document", 
+    tools== [summary_tool]
+)
+
+print(response)
